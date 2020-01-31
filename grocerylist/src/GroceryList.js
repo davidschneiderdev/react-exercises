a94276b78aee37e153c7315c02abb2ec260aad0e
David Schneider <david.jay.schneider@gmail.com>
2020-01-31 16:02:53 -0500

    created click handler that modifies target state

--- grocerylist/src/GroceryList.js
+++ grocerylist/src/GroceryList.js
@@ -1,21 +1,61 @@
 import React from 'react';
 
+// function _handleMouseOver() {
+//     console.log('Item has been moused over.');
+// }
 
-// JSX - the HTML being written inside a React function
-function GroceryList() {
-    return (
-        // You can only return one "thing" inside a function return
-        <div>
-            <h1>Grocery List</h1>
-             <ul>
-                <li>Bread</li>
-                <li>Butter</li>
-                <li>Beer</li>
-            </ul>
-        </div>
-    )
+// const groceries = [
+//     'Bread',
+//     'Butter',
+//     'Beer'
+// ]
+
+class GroceryList extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            groceries: [
+                'Bread',
+                'Butter',
+                'Beer'
+            ]
+        };
+
+    }
+
+    render() {
+        return (
+            // You can only return one "thing" inside a function return
+            <div>
+                <h1>Grocery List</h1>
+                 <ul>
+                    {
+                        this.state.groceries.map((grocery, i) => (
+                            <li onClick={() => {
+                                this._handleClick(i)
+                            }}>{grocery}</li>
+                        ))
+                    }
+                </ul>
+            </div>
+        )
+    }
+
+    _handleClick = (index) => {
+        console.log(index);
+        const newGroceries = [
+            ...this.state.groceries
+        ];
+        newGroceries[index] += '!';
+
+        this.setState({
+            groceries: newGroceries
+        });
+    }
 }
 
+// JSX - the HTML being written inside a React function
 
 export default GroceryList;
 
