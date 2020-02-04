f633e1f9f4d4ca58acca798666f5049365590e31
David Schneider <david.jay.schneider@gmail.com>
2020-02-04 14:28:20 -0500

    adds next character button and error handling

--- react-api-app/src/App.js
+++ react-api-app/src/App.js
@@ -2,25 +2,71 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from 'axios';
+
+const API_ENDPOINT = 'https://swapi.co/api/people/25/';
+
+function urlForId(id) {
+  return `https://swapi.co/api/people/${id}/`
 }
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      currentId: 1
+    }
+  }
+
+  componentDidMount() {
+    axios.get(urlForId(this.state.currentId))
+      .then(response => {
+          console.log(response.data.name);
+          this.setState({
+            name: response.data.name
+          })
+      })
+  }
+
+  render() {
+    
+  
+    return (
+      <div className="App">
+        <header className="App-header">
+          {
+            this.state.name || <img src={logo} />
+          }
+        <button onClick={this._getNextCharacter}>Next Character</button>
+        </header>
+      </div>
+    );
+  }
+
+  _getNextCharacter = () => {
+    this.setState({
+      currentId: this.state.currentId + 1
+    }, () => {
+      console.log(`New currentId is ${this.state.currentId}.`);
+      this._makeAjaxRequest();
+    });
+  }
+
+  _makeAjaxRequest = () => {
+    axios.get(urlForId(this.state.currentId))
+      .then(response => {
+        console.log(response.data.name);
+        this.setState({
+          name: response.data.name
+        })
+      })
+      .catch(err => {
+        this._getNextCharacter();
+      })
+  }
+}
+
 
 export default App;
